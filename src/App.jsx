b49d24d012ff52d3f77cc7c8f98232cd8a62db6a
Geoffroy Periot <geoffroy.periot.dev@gmail.com>
2021-04-08 11:25:05 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  var i = 3;
-  console.log(i)
-  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
         </p>
         <a
           className="App-link"
@@ -18,8 +19,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn 
-          React
+          Learn React
         </a>
       </header>
     </div>
